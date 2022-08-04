ce41d0b42cbfb8611f5d203e2dceda4aef6e28cc
davatata <davatata@gmail.com>
2022-08-04 13:40:41 -0700

    add bootstrap, remove unused files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <div className="App-container">
+        <div className="mx-4 my-2 help-button">
+          <button className="btn btn-secondary">?</button>
+        </div>
+        <h2>Find Islands</h2>
+        <div className='container'>
+          <div className='row'>Row input, col input, setup button</div>
+          <div className='row'>dfs/bfs switch, random button</div>
+          <div className='row'>1x/2x/4x switch</div>
+          <div className='row'>Run Button</div>
+          <div className='row'>Grid</div>
+        </div>
+        
+      </div>
     </div>
   );
 }
