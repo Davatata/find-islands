e5db5f52adc0e3f3106ece951f19b0828245ef1d
davatata <davatata@gmail.com>
2022-08-10 23:05:08 -0700

    coloring works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React, { useState, useEffect, useRef } from 'react';
+import $ from 'jquery';
+import queue from "queue";
 
 function App() {
 
-  const [rows, setRows] = useState(4);
-  const [cols, setCols] = useState(4);
+  const [rows, setRows] = useState(5);
+  const [cols, setCols] = useState(5);
   const [algo, setAlgo] = useState("DFS");
   const [speed, setSpeed] = useState(1);
   const [tempGrid, setTempGrid] = useState([]);
@@ -15,7 +17,6 @@ function App() {
   // Run everytime the rows or cols are changed.
   useEffect(() => {
     console.log("rows and cols changed", rows, cols);
-    console.table("tempGrid: ", tempGrid);
     if (isColsValid() && isRowsValid()) {      
       setup();
     }
@@ -47,9 +48,19 @@ function App() {
 
   const rowsMax = 10;
   const colsMax = 10;
-  const baseSpeed = 200;
-  const waterClasses = ['bg-primary'];
-  const landClasses = ['bg-opacity-50', 'bg-warning'];
+  const baseSpeed = 700;
+  const waterClasses = ['cell', 'bg-primary'];
+  const landClasses = ['cell', 'bg-opacity-50'];
+  const seenLandClasses = ['cell', 'bg-opacity-50', 'bg-danger'];
+  const seenPairs = new Set();
+  const cellQueue = [];
+  const landBgColor = "saddlebrown";
+  const traversedBgColor = "seagreen";
+  
+  let timeoutID = "resetCellTimeout";
+
+  const animationQueue = queue({ autostart: false, concurrency: 1 });
+  
 
   const Algos = Object.freeze({
     DFS:   "DFS",
@@ -57,9 +68,10 @@ function App() {
   });
 
   const Speeds = Object.freeze({
-    Speed1: 1,
-    Speed2: 2,
-    Speed4: 4
+    Speed1_0: 1,
+    Speed2_0: 2,
+    Speed4_0: 4,
+    Speed8_0: 8
   });
 
   function handleRowsChange(ev) {
@@ -104,6 +116,14 @@ function App() {
     return +document.getElementById("ColsInput").value;
   }
 
+  function getInputSpeed() {
+    return +$('input[name="RunSpeed"]:checked').value;
+  }
+
+  function getInputAlgo() {
+    return $('input[name="Algo"]:checked').value;
+  }
+
   function isRowsValid() {
     const rowsValue = getInputRows();
     return rowsValue > 0 && rowsValue <= rowsMax;
@@ -115,19 +135,32 @@ function App() {
   }
 
   function handleAlgoChange(ev) {
-    console.log("algo changed", ev.target.value);
     var algo = ev.target.value;
     setAlgo(algo);
   }
 
   function handleSpeedChange(ev) {
-    console.log("speed changed", +ev.target.value);
     var speed = +ev.target.value;
     setSpeed(speed);
   }
 
   function run() {
+    setTimeout(runTraversal, 100);
+  }
+
+  function runTraversal() {
+    var onesExist = $(".bg-opacity-50");
+
+    // If no 1's in table, return
+    if (onesExist.length === 0) {
+      console.log("No 1's found");
+      endRun();
+      return;
+    }
+
     console.log("run traversal");
+    
+    setIsTraversing(true);
 
     const matches = document.querySelectorAll("td");
 
@@ -142,27 +175,74 @@ function App() {
       }
 
       tempRow.push(matches[i]);
-    }   
+    }    
 
-    // Iterate all the 0's and 1's and perform traversal
-    for (let i = 0; i < cellHolder.length; i++) {
-      for (let j = 0; j < cellHolder[i].length; j++) {
-        if (cellHolder[i][j].textContent === '0') {
-          cellHolder[i][j].classList.remove(...landClasses);
-          cellHolder[i][j].classList.add(...waterClasses);
-        }
-        else {
-          cellHolder[i][j].classList.remove(...waterClasses);
-          cellHolder[i][j].classList.add(...landClasses);
-        }
-      }
+    if (algo === Algos.DFS) {
+      traverseGridDFS(cellHolder);
+    } else if (algo === Algos.BFS) {
+      console.log("traverse BFS");
     }
     
-    setIsTraversing(true);
+    if (cellQueue.length === 0) {
+      console.log("No 1's found: cellQueue was empty.");
+      endRun();
+      return;
+    }
+
+    const animateSpeed = baseSpeed / speed;
+    const elementsToAnimate = [];
+
+    cellQueue.forEach((pair) => {
+      var cell = cellHolder[pair[0]][pair[1]];
+
+      if (cell.textContent === '0') {
+        console.error("Bad cell", pair);
+      } else {
+        elementsToAnimate.push(cell);
+      }
+
+    });    
+
+    for (let i = 0; i < elementsToAnimate.length; i++) {
+      const element = elementsToAnimate[i];
+
+      if (element.textContent === '0') {
+        console.error("0 in the elementsToAnimate")
+      }
+
+      animationQueue.push(() => {
+        return new Promise((resolve) => {
+          if (element.textContent === '1') {
+            waitAnimation(element, resolve);
+            animateElement(element, animateSpeed);
+          }
+
+          if(i === elementsToAnimate.length - 1) {            
+            timeoutID = setTimeout(() => {
+              console.log("Reached end of elementsToAnimate");
+              endRun();
+            }, 1000);
+          }
+        });
+      });
+    }
+
+    animationQueue.start();
+    cellQueue.length = 0;
+    seenPairs.clear();    
   }
 
+
   function endRun() {
-    setIsTraversing(false);
+    console.log("ending run");
+    setTimeout(resetCells, 200);
+  }
+
+  function forceEndRun() {
+    clearTimeout(timeoutID);
+    console.log("force stopping run");
+    animationQueue.end();
+    setTimeout(resetCells, 0);
   }
   
   function randomize() {
@@ -175,9 +255,8 @@ function App() {
         grid[i].push(Math.round(Math.random() * 1));
       }
     }
-    console.log("post randomize grid", grid);
+
     setTempGrid(grid);
-    console.log("post randomize tempGrid", tempGrid);
   }
 
   function setBlank() {
@@ -195,8 +274,6 @@ function App() {
   }
 
   function setup() {
-    console.log("pre-setup tempGrid", tempGrid);
-
     // If grid is empty
     if (tempGrid && tempGrid.length === 0) {
       console.log("grid was empty, filling with 0's");
@@ -253,12 +330,17 @@ function App() {
   function setupFinalGrid() {    
     var finalGridTemp = [];
 
+    setTimeout(() => {
+      resetCells();
+    }, 0);
+    
+
     for(const [row, value] of tempGrid.entries()) {
       var tempRow = [];
 
       for(const [col, v] of value.entries()) {
         let updateClass = v === 0 ? waterClasses.join(" ") : landClasses.join(" ");
-        tempRow.push(<td className={updateClass}
+        tempRow.push(<td style={{backgroundColor: landBgColor}} className={updateClass}
           onClick={() => toggleCell(row,col,v)} key={row+"-"+col}>{v}</td>);
       }
 
@@ -268,18 +350,129 @@ function App() {
     setFinalGrid(finalGridTemp);
   }
 
+  /**
+   * Converts a cell's value.
+   * @param {Specifies which row the cell is on. } row 
+   * @param {Specifies which row the cell is on. } col 
+   * @param {The previous value.} v 
+   */
   function toggleCell(row,col,v) {
     let grid = [...tempGrid];
     grid[row][col] = v === 0 ? 1 : 0;
     setTempGrid(grid);
   }
 
-  function saveGrid(grid) {
-    localStorage.setItem("grid", JSON.stringify(grid));
+  function setTempGridToSelf() {
+    setTempGrid([...tempGrid]);
   }
 
-  function getGrid() {
-    return JSON.parse(localStorage.getItem("grid"));
+  // function saveGrid(grid) {
+  //   localStorage.setItem("grid", JSON.stringify(grid));
+  // }
+
+  // function getGrid() {
+  //   return JSON.parse(localStorage.getItem("grid"));
+  // }
+
+  function traverseGridDFS(cellHolder) {
+    for (let i = 0; i < cellHolder.length; i++) {
+      for (let j = 0; j < cellHolder[i].length; j++) {
+        if (cellHolder[i][j].textContent === '1' && !seenPairs.has(stringPair(i,j))) {
+          seenPairs.add(stringPair(i,j));
+          cellQueue.push([i,j]);
+          islandHelperDFS(cellHolder, i, j);          
+        }
+      }
+    }
+  }
+
+  function stringPair(x, y) {
+    return `${x}_${y}`;
+  }
+
+
+  function islandHelperDFS(cellHolder, i, j) {
+    // Check above current cell
+    if (isValidPair(i+1, j)) {
+      if (cellHolder[i+1][j].textContent === '1' && !seenPairs.has(stringPair(i+1,j))) {
+        seenPairs.add(stringPair(i+1,j));
+        cellQueue.push([i+1,j]);
+        islandHelperDFS(cellHolder, i+1, j);
+      }
+    }
+    // Check right of current cell
+    if (isValidPair(i, j+1)) {
+      if (cellHolder[i][j+1].textContent === '1' && !seenPairs.has(stringPair(i,j+1))) {
+        seenPairs.add(stringPair(i,j+1));
+        cellQueue.push([i,j+1]);
+        islandHelperDFS(cellHolder, i, j+1);
+      }
+    }
+    // Check below current cell
+    if (isValidPair(i-1, j)) {
+      if (cellHolder[i-1][j].textContent === '1' && !seenPairs.has(stringPair(i-1,j))) {
+        seenPairs.add(stringPair(i-1,j));
+        cellQueue.push([i-1,j]);
+        islandHelperDFS(cellHolder, i-1, j);
+      }
+    }
+    // Check left current cell
+    if (isValidPair(i, j-1)) {
+      if (cellHolder[i][j-1].textContent === '1' && !seenPairs.has(stringPair(i,j-1))) {
+        seenPairs.add(stringPair(i,j-1));
+        cellQueue.push([i,j-1]);
+        islandHelperDFS(cellHolder, i, j-1);
+      }
+    }
+  }
+
+  function isValidPair(i, j) {
+    if ( (i >= 0 && i < rows) && (j >= 0 && j < cols) ) {
+      return true;
+    }
+  }
+
+  function waitAnimation(element, callback) {
+    const onAnimationEnd = () => {
+      element.removeEventListener("webkitTransitionEnd", onAnimationEnd);
+      callback();
+    };
+    element.addEventListener("webkitTransitionEnd", onAnimationEnd);
+  };
+
+  function animateElement(element, animateSpeed) {
+    if (!element) return;
+
+    if (element.textContent !== '1') {
+      console.warn("Cell with '0' was added to aniamtion queue", element);
+    } else {
+      element.style.cssText = `      
+        background-color: seagreen !important;
+        transition: background-color ${animateSpeed}ms linear;
+      `;
+    }
+
+
+  };
+
+  function resetCells() {
+    setIsTraversing(false); 
+    cellQueue.length = 0;
+    seenPairs.clear();
+
+    const matches = document.querySelectorAll(`td`);
+
+    matches.forEach(element => {
+      element.style.cssText = '';
+
+      if (element.textContent === '0') {
+        element.classList = waterClasses.join(" ");
+      } else {
+        element.classList = landClasses.join(" ");
+        element.style.backgroundColor = landBgColor;
+      }
+
+    });       
   }
 
   return (
@@ -334,7 +527,7 @@ function App() {
               <div className="btn-group float-start w-100" role="group" aria-label="Basic radio toggle button group" onChange={handleAlgoChange}>
                 <input type="radio" className="btn-check" name="Algo" id="Algo1" 
                   autoComplete="off" value={Algos.DFS} defaultChecked disabled={isTraversing}/>
-                <label className="btn btn-outline-primary" htmlFor="Algo1" title="Depth First Search">{Algos.DFS}</label>
+                <label className="btn btn-outline-primary" htmlFor="Algo1" title="Depth First Search (Up, Right, Down, Left)">{Algos.DFS}</label>
 
                 <input type="radio" className="btn-check" name="Algo" id="Algo2" 
                   autoComplete="off" value={Algos.BFS} disabled={isTraversing}/>
@@ -344,16 +537,20 @@ function App() {
             <div className='col-5 p-0'>
                 <div className="btn-group float-start w-100" role="group" aria-label="Basic radio toggle button group" onChange={handleSpeedChange}>
                   <input type="radio" className="btn-check" name="RunSpeed" id="RunSpeed1" autoComplete="off" defaultChecked 
-                    value={Speeds.Speed1} disabled={isTraversing}/>
-                  <label className="btn btn-outline-primary" htmlFor="RunSpeed1" title="1x Speed">{Speeds.Speed1}x</label>
+                    value={Speeds.Speed1_0} disabled={isTraversing}/>
+                  <label className="btn btn-outline-primary" htmlFor="RunSpeed1" title="1x Speed">{Speeds.Speed1_0}x</label>
 
                   <input type="radio" className="btn-check" name="RunSpeed" id="RunSpeed2" autoComplete="off" 
-                    value={Speeds.Speed2} disabled={isTraversing}/>
-                  <label className="btn btn-outline-primary" htmlFor="RunSpeed2" title="2x Speed">{Speeds.Speed2}x</label>     
+                    value={Speeds.Speed2_0} disabled={isTraversing}/>
+                  <label className="btn btn-outline-primary" htmlFor="RunSpeed2" title="2x Speed">{Speeds.Speed2_0}x</label>     
 
-                  <input type="radio" className="btn-check" name="RunSpeed" id="RunSpeed3" autoComplete="off" 
-                    value={Speeds.Speed4} disabled={isTraversing}/>
-                  <label className="btn btn-outline-primary" htmlFor="RunSpeed3" title="4x Speed">{Speeds.Speed4}x</label>    
+                  <input type="radio" className="btn-check" name="RunSpeed" id="RunSpeed4" autoComplete="off" 
+                    value={Speeds.Speed4_0} disabled={isTraversing}/>
+                  <label className="btn btn-outline-primary" htmlFor="RunSpeed4" title="4x Speed">{Speeds.Speed4_0}x</label>
+
+                  <input type="radio" className="btn-check" name="RunSpeed" id="RunSpeed8" autoComplete="off" 
+                    value={Speeds.Speed8_0} disabled={isTraversing}/>
+                  <label className="btn btn-outline-primary" htmlFor="RunSpeed8" title="8x Speed">{Speeds.Speed8_0}x</label>    
                 </div>
             </div>
           </div>
@@ -387,15 +584,15 @@ function App() {
               {!isTraversing &&               
                 <button className="btn btn-success w-100" onClick={run} 
                 disabled={isTraversing || finalGrid.length === 0} title="Traverse the grid">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-play-fill" viewBox="0 0 16 16">
                   <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
                 </svg>               
                   <span className='mx-1'></span>Run</button>
               }
               {isTraversing &&               
-                <button className="btn btn-warning w-100" onClick={endRun} 
+                <button className="btn btn-warning w-100" onClick={forceEndRun} 
                   title="Stop traversing">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stop-fill" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-stop-fill" viewBox="0 0 16 16">
                     <path d="M5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5A1.5 1.5 0 0 1 5 3.5z"/>
                   </svg>              
                   <span className='mx-1'></span>Stop</button>
